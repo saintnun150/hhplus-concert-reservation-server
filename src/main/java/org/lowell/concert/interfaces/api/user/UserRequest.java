819f4227539c0e2d1be8343f6302f05ca8427929
lowell <saintnun150@gmail.com>
2024-10-11 06:14:18 +0900

    feat: 사용자 관련 Mock API

---
+++ src/main/java/org/lowell/concert/interfaces/api/user/UserRequest.java
@@ -0,0 +1,15 @@
+package org.lowell.concert.interfaces.api.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+public class UserRequest {
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    public static class ChargeAccount {
+        private final Long balance;
+
+    }
+}
