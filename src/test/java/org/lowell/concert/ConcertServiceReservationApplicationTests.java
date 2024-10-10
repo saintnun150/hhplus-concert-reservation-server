b7b74221a3f641670dfb12644c007d06573543d6
lowell <saintnun150@gmail.com>
2024-10-11 06:14:46 +0900

    chore

--- src/test/java/org/lowell/concert/ConcertServiceReservationApplicationTests.java
+++ src/test/java/org/lowell/concert/ConcertServiceReservationApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConcertReservationApplicationTests {
+class ConcertServiceReservationApplicationTests {
 
     @Test
     void contextLoads() {
