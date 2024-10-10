71fb6653e14945a247ca3caaf9d544dd5be745e8
lowell <saintnun150@gmail.com>
2024-10-11 05:41:51 +0900

    feat: 패키지 구조 및 기술 스텍

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,9 +26,11 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("com.mysql:mysql-connector-j")
+//    runtimeOnly("com.mysql:mysql-connector-j")
+    runtimeOnly("com.h2database:h2")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
---
+++ docs/ServerConfiguration.md
@@ -0,0 +1,36 @@
+# 프로젝트 기술 스택
+
+## 주요 기술
+### 1. Java
+- **버전**: 17
+
+### 2. Spring Boot
+- **버전**: 3.3.4
+- **주요 의존성**:
+    - **Spring Data JPA**: DB 접근을 위한 ORM 프레임워크.
+    - **Spring Web**: 스프링 웹 서비스에 필요한 API 제공.
+
+## 빌드 도구
+### Gradle
+- **설명**: 빌드 자동화 도구
+
+## 주요 라이브러리
+
+### 1. Lombok
+- **설명**: 애노테이션을 통한 반복되는 코드 자동완성 기능 제공
+### 2. Spring Boot DevTools
+- **설명**: 코드 변경 시 자동 리로드 개발 편리함
+
+### 3. h2 database
+- **설명**: 데이터베이스와의 연결 JDBC 드라이버.
+- 
+### 4. Springdoc OpenAPI
+- **버전**: 2.5.0
+- **설명**: 자동으로 API 문서를 생성하고 **Swagger UI**를 제공
+
+## 테스트 프레임워크
+### 1. Spring Boot Starter Test
+- **설명**: 통합 테스트 지원 라이브러리
+
+### 2. JUnit Platform Launcher
+- **설명**: JUnit 5 테스트를 실행을 위해 필요
