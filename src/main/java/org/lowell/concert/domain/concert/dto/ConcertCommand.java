d07e23cb56e0172db203fad61a608cb1bf92c5b9
lowell <saintnun150@gmail.com>
2024-10-17 03:12:59 +0900

    feat: 콘서트 생성 및 조회 기능 구현 및 단위 테스트

---
+++ src/main/java/org/lowell/concert/domain/concert/dto/ConcertCommand.java
@@ -0,0 +1,6 @@
+package org.lowell.concert.domain.concert.dto;
+
+public class ConcertCommand {
+    public record Create(String name) {
+    }
+}
---
+++ src/main/java/org/lowell/concert/infra/db/concert/repository/ConcertRepositoryImpl.java
@@ -0,0 +1,42 @@
+package org.lowell.concert.infra.db.concert.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.lowell.concert.application.concert.ConcertMapper;
+import org.lowell.concert.domain.concert.dto.ConcertCommand;
+import org.lowell.concert.domain.concert.model.ConcertInfo;
+import org.lowell.concert.domain.concert.repository.ConcertRepository;
+import org.lowell.concert.infra.db.concert.entity.ConcertEntity;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ConcertRepositoryImpl implements ConcertRepository {
+    private final ConcertJpaRepository jpaRepository;
+    private final ConcertMapper mapper;
+
+    @Override
+    public void createConcert(ConcertCommand.Create command) {
+        ConcertEntity entity = ConcertEntity.builder()
+                                            .name(command.name())
+                                            .createdAt(LocalDateTime.now())
+                                            .build();
+        jpaRepository.save(entity);
+    }
+
+    @Override
+    public ConcertInfo getConcert(long concertId) {
+        ConcertEntity entity = jpaRepository.findById(concertId)
+                                            .orElse(null);
+        return mapper.toPojo(entity);
+    }
+
+
+    @Override
+    public List<ConcertInfo> getConcerts() {
+        List<ConcertEntity> entities = jpaRepository.findAllByDeletedAtIsNull();
+        return mapper.toPojoList(entities);
+    }
+}
