f1653ee33e3894f723ba5a8888205e6115166e92
lowell <saintnun150@gmail.com>
2024-10-25 01:42:58 +0900

    feat: 대기열 토큰 검증 필터, 인터셉터 추가

--- src/main/java/org/lowell/concert/interfaces/api/common/config/FilterConfig.java
+++ src/main/java/org/lowell/concert/interfaces/api/common/config/FilterConfig.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.lowell.concert.interfaces.api.common.filter.WaitingQueueFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +13,17 @@ import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class FilterConfig {
+    private final WaitingQueueFilter waitingQueueFilter;
+
+    public FilterConfig(WaitingQueueFilter waitingQueueFilter) {
+        this.waitingQueueFilter = waitingQueueFilter;
+    }
+
     @Bean
     public FilterRegistrationBean<OncePerRequestFilter> requestAndResponseWrappingFilter() {
         FilterRegistrationBean<OncePerRequestFilter> registrationBean = new FilterRegistrationBean<>();
@@ -33,5 +42,23 @@ public class FilterConfig {
         });
         registrationBean.setOrder(0);
         return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<WaitingQueueFilter> filterRegistrationBean() {
+        FilterRegistrationBean<WaitingQueueFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(waitingQueueFilter);
+        registrationBean.setName("waitingQueueFilter");
+        registrationBean.addUrlPatterns(getFilterUrlPatterns());
+        registrationBean.setOrder(1);
+        return registrationBean;
+    }
+
+    private String[] getFilterUrlPatterns() {
+        String basePath = "/api/v1";
+        List<String> endpoints = Arrays.asList("/payments/*", "/concerts/*", "/waiting-queues/tokens");
+        return endpoints.stream()
+                        .map(endpoint -> basePath + endpoint)
+                        .toArray(String[]::new);
     }
 }
