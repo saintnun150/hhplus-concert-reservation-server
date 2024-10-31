cd11a23e8835475b28ffb75b1354ce1d247a86af
오준영 <junyoung.oh@gooroomee.com>
2024-10-31 13:39:00 +0900

    lettuceConnection redisTemplate

--- src/main/java/org/lowell/concert/infra/redis/config/RedisConfig.java
+++ src/main/java/org/lowell/concert/infra/redis/config/RedisConfig.java
@@ -2,16 +2,38 @@ package org.lowell.concert.infra.redis.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+//        RedisTemplate<String, Object> template = new RedisTemplate<>();
+//        template.setConnectionFactory(connectionFactory);
+//        return template;
+//    }
+
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate( LettuceConnectionFactory lettuceConnectionFactory ) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
+        template.setConnectionFactory( lettuceConnectionFactory );
+
+        // set key serializer
+        // template.setKeySerializer( new StringRedisSerializer(StandardCharsets.UTF_8) ); 와 동일함
+        template.setKeySerializer(RedisSerializer.string() );
+        template.setHashKeySerializer( RedisSerializer.string() );
+
+        // set value serializer
+        // template.setDefaultSerializer( new GenericJackson2JsonRedisSerializer() ); 와 동일함
+        template.setDefaultSerializer( RedisSerializer.json() );
+        template.setValueSerializer( RedisSerializer.json() );
+        template.setHashValueSerializer( RedisSerializer.json() );
+
+        template.afterPropertiesSet();
+
         return template;
     }
 
