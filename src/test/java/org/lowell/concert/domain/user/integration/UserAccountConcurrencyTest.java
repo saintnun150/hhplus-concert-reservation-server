c4104a03d06bd0cf5fdba26042c7d9b5649f43bc
lowell <saintnun150@gmail.com>
2024-11-01 00:23:15 +0900

    feat: 낙관적, 비관적락을 이용한 포인트 충전 동시성 테스트

--- src/test/java/org/lowell/concert/domain/user/integration/UserAccountConcurrencyTest.java
+++ src/test/java/org/lowell/concert/domain/user/integration/UserAccountConcurrencyTest.java
@@ -1,5 +1,6 @@
 package org.lowell.concert.domain.user.integration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,7 @@ import org.lowell.concert.domain.user.service.UserAccountService;
 import org.lowell.concert.infra.db.user.repository.UserAccountJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -20,6 +22,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.lowell.concert.domain.user.exception.UserAccountError.EXCEED_BALANCE;
 
+@Slf4j
 @SpringBootTest
 public class UserAccountConcurrencyTest {
 
@@ -126,5 +129,136 @@ public class UserAccountConcurrencyTest {
 //        assertThat(userAccount.getBalance()).isEqualTo(balance + chargeAmount * chargeCount);
 //    }
 
+    @DisplayName("낙관적 락을 사용해 포인트 충전을 10번 할 경우 한 건만 성공한다.")
+    @Test
+    void attemptChargeWithOptimisticLock10Times() throws InterruptedException {
+        // given
+        Long userId = 1L;
+        long balance = 100;
+        long chargeAmount = 1000;
+        int chargeCount = 10;
+
+        UserAccount account = userAccountJpaRepository.save(UserAccount.builder()
+                                                                       .userId(userId)
+                                                                       .balance(balance)
+                                                                       .build());
+        // when
+        ExecutorService executorService = Executors.newFixedThreadPool(chargeCount);
+        CountDownLatch latch = new CountDownLatch(chargeCount);
+
+        AtomicInteger success = new AtomicInteger(0);
+        AtomicInteger failed = new AtomicInteger(0);
+
+        for (int i = 0; i < chargeCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    accountService.chargeBalance(new UserAccountCommand.Action(userId, chargeAmount));
+                    success.incrementAndGet();
+                } catch (Exception e) {
+                    failed.incrementAndGet();
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+        executorService.shutdown();
+
+        // then
+        assertThat(success.get()).isEqualTo(1);
+        UserAccount userAccount = accountService.getUserAccount(userId);
+        assertThat(userAccount.getBalance()).isEqualTo(balance + chargeAmount * success.get());
+    }
+
+
+    @DisplayName("낙관적 락을 사용해 포인트 충전을 1000번 할 경우 모두 성공한다.")
+    @Test
+    void attemptChargeWithOptimisticLock1000Times() throws InterruptedException {
+        Long userId = 1L;
+        long balance = 100;
+        long chargeAmount = 1000;
+        int chargeCount = 1000;
+
+        UserAccount account = userAccountJpaRepository.save(UserAccount.builder()
+                                                                       .userId(userId)
+                                                                       .balance(balance)
+                                                                       .build());
+        ExecutorService executorService = Executors.newFixedThreadPool(chargeCount);
+        CountDownLatch latch = new CountDownLatch(chargeCount);
+
+        AtomicInteger success = new AtomicInteger(0);
+        AtomicInteger failed = new AtomicInteger(0);
+
+        for (int i = 0; i < chargeCount; i++) {
+            executorService.submit(() -> {
+                int maxRetries = 100;
+                int retryCount = 0;
+                boolean isCharged = false;
+
+                while (retryCount < maxRetries && !isCharged) {
+                    try {
+                        accountService.chargeBalance(new UserAccountCommand.Action(userId, chargeAmount));
+                        success.incrementAndGet();
+                        isCharged = true;
+                    } catch (ObjectOptimisticLockingFailureException oe) {
+                        retryCount++;
+                        log.warn("## retry attempt:[{}]", retryCount);
+                    } catch (Exception e) {
+                        log.warn("## charge error:[{}]", e.getClass().getSimpleName(), e);
+                        failed.incrementAndGet();
+                        break;
+                    }
+                }
+                latch.countDown();
+            });
+        }
+        latch.await();
+        executorService.shutdown();
+
+        // then
+        assertThat(success.get()).isEqualTo(1000);
+        UserAccount userAccount = accountService.getUserAccount(userId);
+        assertThat(userAccount.getBalance()).isEqualTo(balance + chargeAmount * success.get());
+    }
+
+    @DisplayName("비관적 락을 사용해 포인트 충전을 1000번 할 경우 모두 성공한다.")
+    @Test
+    void attemptChargeWithPessimisticLock100Times() throws InterruptedException {
+        Long userId = 1L;
+        long balance = 100;
+        long chargeAmount = 1000;
+        int chargeCount = 1000;
+
+        UserAccount account = userAccountJpaRepository.save(UserAccount.builder()
+                                                                       .userId(userId)
+                                                                       .balance(balance)
+                                                                       .build());
+        ExecutorService executorService = Executors.newFixedThreadPool(chargeCount);
+        CountDownLatch latch = new CountDownLatch(chargeCount);
+
+        AtomicInteger success = new AtomicInteger(0);
+        AtomicInteger failed = new AtomicInteger(0);
+
+        for (int i = 0; i < chargeCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    accountService.chargeBalanceWithLock(new UserAccountCommand.Action(userId, chargeAmount));
+                    success.incrementAndGet();
+                } catch (Exception e) {
+                    log.warn("## charge error:[{}]", e.getClass().getSimpleName(), e);
+                    failed.incrementAndGet();
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+        executorService.shutdown();
+
+        // then
+        assertThat(success.get()).isEqualTo(1000);
+        UserAccount userAccount = accountService.getUserAccount(userId);
+        assertThat(userAccount.getBalance()).isEqualTo(balance + chargeAmount * success.get());
+    }
 
 }
