cf110ed4d8f51afe8b62a3614ac49cfa19142af7
lowell <saintnun150@gmail.com>
2024-10-31 23:01:01 +0900

    feat: 테스트 환경에서 롬복 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
