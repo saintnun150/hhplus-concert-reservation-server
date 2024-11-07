9b9a4b008912ddbd587efb9124372ba53c9ee67e
lowell <saintnun150@gmail.com>
2024-11-07 20:58:25 +0900

    feat: redis 자료구조 추가

--- src/main/java/org/lowell/concert/infra/redis/support/RedisRepository.java
+++ src/main/java/org/lowell/concert/infra/redis/support/RedisRepository.java
@@ -2,8 +2,10 @@ package org.lowell.concert.infra.redis.support;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 @Repository
@@ -11,11 +13,42 @@ import java.util.concurrent.TimeUnit;
 public class RedisRepository {
     private final RedisTemplate<String, Object> redisTemplate;
 
-    public Boolean setIfAbsent(String key, String value, Long leaseTime, TimeUnit timeUnit){
+    public Boolean addIfAbsent(String key, String value, Long leaseTime, TimeUnit timeUnit){
         return redisTemplate.opsForValue().setIfAbsent(key, value, leaseTime, timeUnit);
     }
 
-    public Boolean delete(String key){
+    public Boolean deleteKey(String key){
         return redisTemplate.delete(key);
+    }
+    public Boolean existsKey(String key) {
+        return redisTemplate.hasKey(key);
+    }
+
+    public Boolean addZSet(String key, String value, double score){
+        return redisTemplate.opsForZSet().add(key, value, score);
+    }
+
+    public Boolean addZSetIfAbsent(String key, String value, double score){
+        return redisTemplate.opsForZSet().addIfAbsent(key, value, score);
+    }
+
+    public ZSetOperations<String, Object> getZSetOps() {
+        return redisTemplate.opsForZSet();
+    }
+
+    public Set<Object> getZSetRange(String key, long start, long end) {
+        return this.getZSetOps().range(key, start, end);
+    }
+
+    public Long getZSetRank(String key, Object value) {
+        return this.getZSetOps().rank(key, value);
+    }
+
+    public Long getZSetSize(String key) {
+        return this.getZSetOps().zCard(key);
+    }
+
+    public Set<ZSetOperations.TypedTuple<Object>> zPopMin(String key, long count) {
+        return this.getZSetOps().popMin(key, count);
     }
 }
